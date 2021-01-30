de72f8bfa4d13c0f9e1a2e3048012f105488d95f
0is2 <youngneee@gmail.com>
2021-01-31 01:19:08 +0900

    returned, modeling 원복

--- gdmarket/item/src/main/java/gdmarket/ItemRegistered.java
+++ gdmarket/item/src/main/java/gdmarket/ItemRegistered.java
@@ -7,15 +7,11 @@ public class ItemRegistered extends AbstractEvent {
     private String itemStatus;
     private Integer itemPrice;
     private String rentalStatus;
-    private Integer reservationNo;
 
     public Integer getItemNo() {
         return itemNo;
     }
-
-    public void setItemNo(Integer itemNo) {
-        this.itemNo = itemNo;
-    }
+    public void setItemNo(Integer itemNo) { this.itemNo = itemNo; }
     public String getItemName() {
         return itemName;
     }
@@ -34,16 +30,11 @@ public class ItemRegistered extends AbstractEvent {
     public void setItemPrice(Integer itemPrice) {
         this.itemPrice = itemPrice;
     }
-    private String getRentalStatus () {
+    public String getRentalStatus() {
         return rentalStatus;
     }
-    public void setRentalStatus (String rentalStatus){
+    public void setRentalStatus(String rentalStatus) {
         this.rentalStatus = rentalStatus;
     }
-    private Integer getReservationNo() {
-        return reservationNo;
-    }
-    public void setReservationNo (Integer reservationNo){
-        this.reservationNo = reservationNo;
-    }
+
 }
