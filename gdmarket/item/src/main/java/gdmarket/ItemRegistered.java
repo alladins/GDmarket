a09cdb6f82182dbef13f6ec5d2a4b98db0dff084
HyemiJang <4dhang@gmail.com>
2021-01-30 21:53:51 +0900

    Update ItemRegistered.java

--- gdmarket/item/src/main/java/gdmarket/ItemRegistered.java
+++ gdmarket/item/src/main/java/gdmarket/ItemRegistered.java
@@ -6,6 +6,8 @@ public class ItemRegistered extends AbstractEvent {
     private String itemName;
     private String itemStatus;
     private Integer itemPrice;
+    private String rentalStatus;
+    private Integer reservationNo;
 
     public Integer getItemNo() {
         return itemNo;
@@ -17,22 +19,31 @@ public class ItemRegistered extends AbstractEvent {
     public String getItemName() {
         return itemName;
     }
-
     public void setItemName(String itemName) {
         this.itemName = itemName;
     }
     public String getItemStatus() {
         return itemStatus;
     }
-
     public void setItemStatus(String itemStatus) {
         this.itemStatus = itemStatus;
     }
     public Integer getItemPrice() {
         return itemPrice;
     }
-
     public void setItemPrice(Integer itemPrice) {
         this.itemPrice = itemPrice;
+    }
+    private String getRentalStatus () {
+        return rentalStatus;
+    }
+    public void setRentalStatus (String rentalStatus){
+        this.rentalStatus = rentalStatus;
+    }
+    private Integer getReservationNo() {
+        return reservationNo;
+    }
+    public void setReservationNo (Integer reservationNo){
+        this.reservationNo = reservationNo;
     }
 }
